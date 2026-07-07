6c8b3d94ae15facb4c2a7899c932ae70e1bbd8a7
wenping29 <wenping29@163.com>
2026-07-07 20:02:33 +0800

    asd

--- backend/src/Helpers/PasswordHelper.cs
+++ backend/src/Helpers/PasswordHelper.cs
@@ -5,16 +5,15 @@ namespace CesiumApi.Helpers;
 public static class PasswordHelper
 {
     private static readonly PasswordHasher<object> _passwordHasher = new();
-    private static readonly object? _nullUser = null;
 
     public static string HashPassword(string password)
     {
-        return _passwordHasher.HashPassword(_nullUser, password);
+        return _passwordHasher.HashPassword(null!, password);
     }
 
     public static bool VerifyPassword(string password, string storedHash)
     {
-        return _passwordHasher.VerifyHashedPassword(_nullUser, storedHash, password) 
+        return _passwordHasher.VerifyHashedPassword(null!, storedHash, password) 
             != PasswordVerificationResult.Failed;
     }
 }
