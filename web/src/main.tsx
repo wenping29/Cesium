4dcdead99a995806086ecf74dad4adbfdf5d6c0b
wenping29 <wenping29@163.com>
2026-07-09 21:07:47 +0800

    优化bug

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,11 @@
+import './mock'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import { store } from './store'
 import router from './router'
-
-import './mock'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- web/src/mock/index.ts
+++ web/src/mock/index.ts
@@ -5,8 +5,3 @@ import './auth'
 Mock.setup({
   timeout: '200-600',
 })
-
-console.log('Mockjs initialized successfully')
-console.log('Mocked routes:', Object.keys((Mock as any)._mocked || {}))
-
-export default Mock
